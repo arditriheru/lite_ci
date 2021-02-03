3ccc507a2edf0d606708ea2f52c5bb80cd1ff60e
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-02-03 10:09:59 +0700

    Add files via upload

--- application/controllers/swab/DataSwab.php
+++ application/controllers/swab/DataSwab.php
@@ -3,22 +3,17 @@
 class dataSwab extends CI_Controller
 {
 
-	private $imagepath;
 	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model("mSimetris");
 		$this->load->library('form_validation');
-		$this->imagepath = realpath(APPPATH.'./uploads/');
-		$this->load->helper('url');
 	}
 
 	public function index()
 	{
-		$data['title'] 		= "Swab Antigen";
+		$data['title'] 		= "Daftar Swab Antigen";
 		$data['subtitle'] 	= "";
-
-		session_destroy();
 
 		$this->load->view('templates/header',$data);
 		$this->load->view('swab/vDaftarSwab',$data);
@@ -51,9 +46,51 @@ class dataSwab extends CI_Controller
 		$tanggal 			= getDatenow();
 		$jam 				= getTimenow();
 
+		$userdata = array(
+			'no_identitas' 		=> $no_identitas,
+			'nama' 				=> $nama,
+			'alamat' 			=> $alamat,
+			'tgl_lahir' 		=> $tgl_lahir,
+			'id_sex' 			=> $id_sex,
+			'id_jadwal_swab' 	=> $id_jadwal_swab,
+			'kontak' 			=> $kontak,
+			'email' 			=> $email,
+			'no_invoice' 		=> "inv".uniqid(),
+			'tanggal' 			=> $tanggal,
+			'jam' 				=> $jam
+		);
+
+		if($userdata)
+		{
+			$this->session->set_userdata($userdata);
+			$this->session->set_flashdata('alert','<div class="alert alert-success alert-dismissable">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+				Berhasil menambahkan identitas</div>');
+			redirect('swab/dataSwab/formKtp');
+		}else{
+			$this->session->set_flashdata('alert','<div class="alert alert-danger alert-dismissable">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+				Gagal menambahkan identitas</div>');
+			redirect('swab/dataSwab/formIdentitas');
+		}
+
+	}
+
+	public function formKtp()
+	{
+		$data['title'] 		= "Upload Kartu Identitas";
+		$data['subtitle'] 	= "";
+
+		$this->load->view('templates/header',$data);
+		$this->load->view('swab/vFormKtp',$data);
+		$this->load->view('templates/footer',$data);
+	}
+
+	public function formKtpAksi()
+	{
 		$file_name 						= 'id_'.$this->input->post('id_booking_swab').date('Ymd').uniqid();
 		$config['upload_path']          = './uploads/';
-		$config['allowed_types']        = 'gif|jpg|png';
+		$config['allowed_types']        = 'jpg|jpeg|png';
 		$config['file_name']        	= $file_name;
 		$config['overwrite']			= true;
 		$config['max_size']             = 3000;
@@ -66,68 +103,23 @@ class dataSwab extends CI_Controller
 			$file_identitas	= implode(['file_identitas' => $fileData['file_name']]);
 
 			$userdata = array(
-				'no_identitas' 		=> $no_identitas,
-				'nama' 				=> $nama,
-				'alamat' 			=> $alamat,
-				'tgl_lahir' 		=> $tgl_lahir,
-				'id_sex' 			=> $id_sex,
-				'id_jadwal_swab' 	=> $id_jadwal_swab,
-				'kontak' 			=> $kontak,
-				'email' 			=> $email,
 				'file_identitas'	=> $file_identitas,
-				'tanggal' 			=> $tanggal,
-				'jam' 				=> $jam
 			);
 
 			$this->session->set_userdata($userdata);
+
 			$this->session->set_flashdata('alert','<div class="alert alert-success alert-dismissable">
 				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-				Berhasil menambahkan identitas</div>');
+				Berhasil upload kartu identitas</div>');
 			redirect('swab/dataSwab/formNota');
+		}else{
+			$this->session->set_flashdata('alert','<div class="alert alert-danger alert-dismissable">
+				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+				Gagal upload. Silahkan upload file jpg/jpeg/png</div>');
+			redirect('swab/dataSwab/formKtp');
 		}
 
 	}
-
-	// public function uploadSubmit()
-	// {	
-	// 	if(!empty($_FILES['image_up']['name']))
-	// 	{ 	
-	// 		$file_name 						= 'id_'.$this->input->post('id_booking_swab').date('Ymd').uniqid();
-	// 		$config['upload_path']          = './uploads/';
-	// 		$config['allowed_types']        = 'gif|jpg|png';
-	// 		$config['file_name']        	= $file_name;
-	// 		$config['overwrite']			= true;
-	// 		$config['max_size']             = 3000;
-
-	// 		$this->load->library('upload', $config);
-
-	// 		if ($this->upload->do_upload('image_up'))
-	// 		{
-	// 			$fileData 		= $this->upload->data();
-	// 			$file_identitas	= implode(['file_identitas' => $fileData['file_name']]);
-
-	// 			$userdata = array(
-	// 				'no_identitas' 		=> $no_identitas,
-	// 				'nama' 				=> $nama,
-	// 				'alamat' 			=> $alamat,
-	// 				'tgl_lahir' 		=> $tgl_lahir,
-	// 				'id_sex' 			=> $id_sex,
-	// 				'id_jadwal_swab' 	=> $id_jadwal_swab,
-	// 				'kontak' 			=> $kontak,
-	// 				'email' 			=> $email,
-	// 				'file_identitas'	=> $file_identitas,
-	// 				'tanggal' 			=> $tanggal,
-	// 				'jam' 				=> $jam
-	// 			);
-	// 			$this->session->set_userdata($userdata);
-	// 			$this->session->set_flashdata('alert','<div class="alert alert-success alert-dismissable">
-	// 				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-	// 				Berhasil menambahkan identitas</div>');
-	// 			redirect('swab/dataSwab/formNota');
-	// 		}
-	// 	}	
-	// }
-
 
 	public function formNota()
 	{
@@ -143,7 +135,7 @@ class dataSwab extends CI_Controller
 	{
 		$file_name 						= 'nota_'.$this->input->post('id_booking_swab').date('Ymd').uniqid();
 		$config['upload_path']          = './uploads/';
-		$config['allowed_types']        = 'gif|jpg|png';
+		$config['allowed_types']        = 'jpg|jpeg|png';
 		$config['file_name']        	= $file_name;
 		$config['overwrite']			= true;
 		$config['max_size']             = 3000;
@@ -156,42 +148,58 @@ class dataSwab extends CI_Controller
 			$file_pembayaran	= implode(['file_pembayaran' => $fileData['file_name']]);
 
 			$data = array(
-				'no_identitas' 		=> $this->session->userdata("no_identitas"),
-				'nama' 				=> $this->session->userdata("nama"),
-				'alamat' 			=> $this->session->userdata("alamat"),
-				'tgl_lahir' 		=> $this->session->userdata("tgl_lahir"),
-				'id_sex' 			=> $this->session->userdata("id_sex"),
-				'id_jadwal_swab' 	=> $this->session->userdata("id_jadwal_swab"),
-				'kontak' 			=> $this->session->userdata("kontak"),
-				'email' 			=> $this->session->userdata("email"),
-				'file_identitas'	=> $this->session->userdata("file_identitas"),
-				'file_pembayaran'	=> $file_pembayaran,
-				'no_invoice' 		=> "inv".uniqid(),
-				'tanggal' 			=> $this->session->userdata("tanggal"),
-				'jam' 				=> $this->session->userdata("jam"),
+				'no_identitas' 		=> $this->session->userdata('no_identitas'),
+				'nama' 				=> $this->session->userdata('nama'),
+				'alamat' 			=> $this->session->userdata('alamat'),
+				'tgl_lahir' 		=> $this->session->userdata('tgl_lahir'),
+				'id_sex' 			=> $this->session->userdata('id_sex'),
+				'id_jadwal_swab' 	=> $this->session->userdata('id_jadwal_swab'),
+				'kontak' 			=> $this->session->userdata('kontak'),
+				'email' 			=> $this->session->userdata('email'),
+				'file_identitas' 	=> $this->session->userdata('file_identitas'),
+				'file_pembayaran' 	=> $file_pembayaran,
+				'no_invoice' 		=> $this->session->userdata('no_invoice'),
+				'tanggal' 			=> $this->session->userdata('tanggal'),
+				'jam' 				=> $this->session->userdata('jam')
 			);
 
-			$insert = $this->mSimetris->insertData('booking_swab',$data);
-			$this->session->set_flashdata('alert','<div class="alert alert-success alert-dismissable">
+			$this->mSimetris->insertData('booking_swab',$data);
+
+			$where = array(
+				'no_identitas' 	=> $this->session->userdata('no_identitas'),
+				'jam' 			=> $this->session->userdata('jam')
+			);
+
+			$data = $this->mSimetris->selectData('booking_swab','id_booking_swab',$where)->result();
+
+			foreach ($data as $d) {
+				$id = $d->id_booking_swab;
+			}
+
+			redirect('swab/dataSwab/detail/'.$id);
+		}else{
+			$this->session->set_flashdata('alert','<div class="alert alert-danger alert-dismissable">
 				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-				Berhasil mendaftar test SWAB Antigen</div>');
-			redirect('swab/dataSwab/detail');
+				Gagal upload. Silahkan upload file jpg/jpeg/png</div>');
+			redirect('swab/dataSwab/formNota');
 		}
 
 	}
 
-	public function detail()
+	public function detail($id)
 	{
-		$data['title'] 		= "Upload Bukti Pembayaran";
+		$data['title'] 		= "Detail Daftar";
 		$data['subtitle'] 	= "";
 
 		$where = array(
-			'no_identitas' 	=> $this->session->userdata('no_identitas'),
-			'jam' 			=> $this->session->userdata('jam'),
+			'id_booking_swab' 	=> $id,
 		);
 
-		$data['data'] = $this->mSimetris->dataSwab($where)->result();
-
+		if($where)
+		{
+			$data['data'] = $this->mSimetris->dataSwabDetail($where)->result();
+		}
+		
 		$this->load->view('templates/header',$data);
 		$this->load->view('swab/vDetail',$data);
 		$this->load->view('templates/footer',$data);
