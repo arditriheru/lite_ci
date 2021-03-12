80144b252fe1176e90d284eed8d4e91c831cdaac
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-03-12 15:16:42 +0700

    Add files via upload

--- application/views/app/vDataJadwal.php
+++ application/views/app/vDataJadwal.php
@@ -21,7 +21,7 @@
                                 <div class="row form-group">
                                     <div class="col col-md-12">
                                         <div class="input-group">
-                                         <select name="id_dokter" id="id_dokter" class=" form-control" required="">
+                                           <select name="id_dokter" id="id_dokter" class=" form-control" required="">
                                             <option value="">Pilih Dokter</option>
                                             <?php foreach ($datadokter as $d) : ?>
                                                 <option value="<?php echo $d->id_dokter ?>"><?php echo $d->nama_unit.' - '.$d->nama_dokter ?></option>
@@ -35,97 +35,126 @@
                         <table class="table table-striped">
                           <thead>
                             <tr>
-                                <th scope="col"><center>Hari</center></th>
                                 <th scope="col"><center>Nama Dokter</center></th>
-                                <th scope="col"><center>Pukul</center></th>
+                                <th scope="col"><center>Hari, Jam</center></th>
+                                <th scope="col"><center>Kuota</center></th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php $no=1; foreach ($datajadwal as $d) : ?>
                             <tr>
-                                <td><center><?php echo $d->nama_hari; ?></center></td>
                                 <td><left><?php echo $d->nama_unit.' - '.$d->nama_dokter; ?></left></td>
-                                <td><center><?php echo $d->jam; ?></center></td>
+                                <td><center><?php echo $d->nama_hari.', '.$d->jam; ?></center></td>
+                                <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+
                             </tr>
                         <?php endforeach ?>
                     </tbody>
                 </table>
-
-            <?php }else{ ?>
-
-                <div class="col-md-12">
-                    <div align="center" class="card">
-                        <div class="card-header">
-                            <strong class="card-title">Dokter <?php echo $nama_dokter ?></strong>
-                        </div>
-                        <div class="card-body">
-                           <table class="table table-striped">
-                              <thead>
-                                <tr>
-                                    <th scope="col"><center>Hari</center></th>
-                                    <th scope="col"><center>Pukul</center></th>
-                                    <th scope="col"><center>Kuota</center></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($datasenin as $d) : ?>
-                                    <tr>
-                                        <td><center>Senin</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                                <?php foreach ($dataselasa as $d) : ?>
-                                    <tr>
-                                        <td><center>Selasa</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                                <?php foreach ($datarabu as $d) : ?>
-                                    <tr>
-                                        <td><center>Rabu</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                                <?php foreach ($datakamis as $d) : ?>
-                                    <tr>
-                                        <td><center>Kamis</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                                <?php foreach ($datajumat as $d) : ?>
-                                    <tr>
-                                        <td><center>Jumat</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                                <?php foreach ($datasabtu as $d) : ?>
-                                    <tr>
-                                        <td><center>Sabtu</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                                <?php foreach ($dataminggu as $d) : ?>
-                                    <tr>
-                                        <td><center>Minggu</center></td>
-                                        <td><left><?php echo $d->jam.$d->ims ?></left></td>
-                                        <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
-                                    </tr>
-                                <?php endforeach ?>
-                            </tbody>
-                        </table>
-                    </div>
+                <hr>
+                <br>
+                <div align="center" class="card-title">
+                    <strong class="text-center">Poliklinik Libur</strong>
                 </div>
-            </div>
+                <hr>
+                <table class="table table-striped">
+                  <thead>
+                    <tr>
+                        <th scope="col"><center>Nama Dokter</center></th>
+                        <th scope="col"><center>Tanggal</center></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php if(empty($datajadwallibur)){ ?>
+                        <tr>
+                            <td colspan="3"><center><p class="text-danger">Belum ada jadwal poli libur</p></center></td>
+                        </tr>
+                    <?php }else{ 
+                        $no=1; foreach ($datajadwallibur as $d) : ?>
+                        <tr>
+                            <td><left><p class="text-danger"><?php echo $d->nama_unit.' - '.$d->nama_dokter; ?></p></left></td>
+                            <td><center><p class="text-danger"></font><?php echo formatDateIndo($d->tanggal).' (Poli Libur)'; ?></p></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                <?php } ?>
+            </tbody>
+        </table>
 
-        <?php } ?>
+    <?php }else{ ?>
 
+        <div class="col-md-12">
+            <div align="center" class="card">
+                <div class="card-header">
+                    <strong class="card-title">Dokter <?php echo $nama_dokter ?></strong>
+                </div>
+                <div class="card-body">
+                 <table class="table table-striped">
+                  <thead>
+                    <tr>
+                        <th scope="col"><center>Hari</center></th>
+                        <th scope="col"><center>Pukul</center></th>
+                        <th scope="col"><center>Kuota</center></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($datasenin as $d) : ?>
+                        <tr>
+                            <td><center>Senin</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                    <?php foreach ($dataselasa as $d) : ?>
+                        <tr>
+                            <td><center>Selasa</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                    <?php foreach ($datarabu as $d) : ?>
+                        <tr>
+                            <td><center>Rabu</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                    <?php foreach ($datakamis as $d) : ?>
+                        <tr>
+                            <td><center>Kamis</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                    <?php foreach ($datajumat as $d) : ?>
+                        <tr>
+                            <td><center>Jumat</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                    <?php foreach ($datasabtu as $d) : ?>
+                        <tr>
+                            <td><center>Sabtu</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                    <?php foreach ($dataminggu as $d) : ?>
+                        <tr>
+                            <td><center>Minggu</center></td>
+                            <td><left><?php echo $d->jam.$d->ims ?></left></td>
+                            <td><center><?php echo $d->kuota.' Pasien'; ?></center></td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
     </div>
+</div>
+
+<?php } ?>
+
+</div>
 </div> <!-- .card -->
 </div>
 </div>
